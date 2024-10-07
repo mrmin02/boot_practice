0ecb45f9070e0b29ab6e25d815a03aeffc5ba2f7
cms <mrmin02@naver.com>
2024-10-07 09:24:43 +0900

    feat: actuator 추가

--- src/main/java/minseok/practice/cms/CmsApplication.java
+++ src/main/java/minseok/practice/cms/CmsApplication.java
@@ -1,13 +1,20 @@
 package minseok.practice.cms;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CmsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CmsApplication.class, args);
+	}
+
+	@Bean
+	public InMemoryHttpExchangeRepository httpExchangeRepository() {
+		return new InMemoryHttpExchangeRepository();
 	}
 
 }
